d0d5e840d55596238d0a4ad838aecc608e7fd699
Ruberto Bruno Soares Martins <ruberto.martins@globalhitss.com.br>
2025-07-25 11:34:57 -0300

    Versão inicial do projeto SIG FO

--- projetos/models.py
+++ projetos/models.py
@@ -1,56 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Projeto(models.Model):
-    # Seção 1: Identificação
-    Identificador = models.CharField(max_length=100, unique=True, primary_key=True)
+    Identificador = models.CharField(max_length=100, primary_key=True, unique=True)
     Ano_Implantacao = models.IntegerField(null=True, blank=True)
     Regional = models.CharField(max_length=100, null=True, blank=True)
     Estacao_BdRaf = models.CharField(max_length=100, null=True, blank=True)
     COD_IBGE = models.IntegerField(null=True, blank=True)
-    Municipio = models.CharField(max_length=200, null=True, blank=True)
-
-    # Seção 2: CAPEX e Qualidade
-    Projeto_CAPEX = models.CharField(max_length=200, null=True, blank=True)
-    Sub_Projeto_CAPEX = models.CharField(max_length=200, null=True, blank=True)
+    Municipio = models.CharField(max_length=100, null=True, blank=True)
+    Projeto_CAPEX = models.CharField(max_length=100, null=True, blank=True)
+    Sub_Projeto_CAPEX = models.CharField(max_length=100, null=True, blank=True)
     SI_CAPEX = models.CharField(max_length=100, null=True, blank=True)
-    Reserva_1 = models.CharField(max_length=200, null=True, blank=True)
-    Reserva_2 = models.CharField(max_length=200, null=True, blank=True)
-    Reserva_3 = models.CharField(max_length=200, null=True, blank=True)
-    Reserva_4 = models.CharField(max_length=200, null=True, blank=True)
-    Reserva_5 = models.CharField(max_length=200, null=True, blank=True)
-    QUALIDADE_PREENCHIMENTO = models.CharField(max_length=50, null=True, blank=True)
     CAPEX = models.FloatField(null=True, blank=True)
     OS_WF = models.CharField(max_length=100, null=True, blank=True)
+    QUALIDADE_PREENCHIMENTO = models.CharField(max_length=50, null=True, blank=True)
 
-    # Seção 3: Cronograma e Métricas
+    Req_Gen_Plan = models.DateField(null=True, blank=True)
+    Req_Gen_Real = models.DateField(null=True, blank=True)
+    Projeto_Executivo_Plan = models.DateField(null=True, blank=True)
+    Projeto_Executivo_Real = models.DateField(null=True, blank=True)
+    Licenciamento_Plan = models.DateField(null=True, blank=True)
+    Licenciamento_Real = models.DateField(null=True, blank=True)
+    MOS_Plan = models.DateField(null=True, blank=True)
+    MOS_Real = models.DateField(null=True, blank=True)
+    Swap_Plan = models.DateField(null=True, blank=True)
+    Swap_Real = models.DateField(null=True, blank=True)
+    Construcao_Total_Plan = models.DateField(null=True, blank=True)
+    Construcao_Total_Real = models.DateField(null=True, blank=True)
+    RFI_Plan = models.DateField(null=True, blank=True)
+    RFI_Real = models.DateField(null=True, blank=True)
+    Entroncado_Plan = models.DateField(null=True, blank=True)
+    Entroncado_Real = models.DateField(null=True, blank=True)
+    Documentacao_Plan = models.DateField(null=True, blank=True)
+    Documentacao_Real = models.DateField(null=True, blank=True)
+    GP_Plan = models.DateField(null=True, blank=True)
+    GP_Real = models.DateField(null=True, blank=True)
+    Data_Protocolo_Real = models.DateField(null=True, blank=True)
+    Construcao_Parcial_Real = models.DateField(null=True, blank=True)
+
     Km_Projeto_Executado = models.FloatField(null=True, blank=True)
     Km_Construido = models.FloatField(null=True, blank=True)
-    Data_Protocolo_Real = models.CharField(max_length=50, default='Não realizado')
-    Construcao_Parcial_Real = models.CharField(max_length=50, default='Não realizado')
 
-    # Marcos Planejados vs. Reais
-    Req_Gen_Plan = models.DateField(null=True, blank=True)
-    Req_Gen_Real = models.CharField(max_length=50, default='Não realizado')
-    Projeto_Executivo_Plan = models.DateField(null=True, blank=True)
-    Projeto_Executivo_Real = models.CharField(max_length=50, default='Não realizado')
-    Licenciamento_Plan = models.DateField(null=True, blank=True)
-    Licenciamento_Real = models.CharField(max_length=50, default='Não realizado')
-    MOS_Plan = models.DateField(null=True, blank=True)
-    MOS_Real = models.CharField(max_length=50, default='Não realizado')
-    Swap_Plan = models.DateField(null=True, blank=True)
-    Swap_Real = models.CharField(max_length=50, default='Não realizado')
-    Construcao_Total_Plan = models.DateField(null=True, blank=True)
-    Construcao_Total_Real = models.CharField(max_length=50, default='Não realizado')
-    RFI_Plan = models.DateField(null=True, blank=True)
-    RFI_Real = models.CharField(max_length=50, default='Não realizado')
-    Entroncado_Plan = models.DateField(null=True, blank=True)
-    Entroncado_Real = models.CharField(max_length=50, default='Não realizado')
-    Documentacao_Plan = models.DateField(null=True, blank=True)
-    Documentacao_Real = models.CharField(max_length=50, default='Não realizado')
-    GP_Plan = models.DateField(null=True, blank=True)
-    GP_Real = models.CharField(max_length=50, default='Não realizado')
-
-    dataAtualizacao = models.DateField(auto_now=True)
+    data_atualizacao = models.DateField(auto_now=True)
+    ultima_atualizacao_por = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.Identificador
--- sigfo_backend/urls.py
+++ sigfo_backend/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from projetos.views import index
+from rest_framework.routers import DefaultRouter
+from projetos.views import login_view, index_view, admin_view, handle_login, handle_logout, ProjetoViewSet
+
+router = DefaultRouter()
+router.register(r'projetos', ProjetoViewSet, basename='projeto')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('projetos.urls')),
-    path('', index, name='index'),
+    path('', login_view, name='login'),
+    path('app/', index_view, name='app'),
+    path('painel/', admin_view, name='painel'),
+    path('api/login/', handle_login, name='api_login'),
+    path('api/logout/', handle_logout, name='api_logout'),
+
+    # Adicione esta linha para as rotas da API de dados
+    path('api/', include(router.urls)),
 ]
